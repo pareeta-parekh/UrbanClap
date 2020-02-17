aac688a2ca4742e76a1a2a295e653102f0d02342
Bhautik Jivani <jivanibhautik@gmail.com>
2020-02-17 13:08:12 +0530

    UrbanClap v9

---
+++ serviceProvider/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .users import *
+from .services import *
+from .chat import *
+
+urlpatterns = [
+    path('register/', spregister),
+    path('login/', sprlogin),
+    path('logout/', sprlogout),
+    path('addservice/', addservice),
+    path('appliedservices/',appliedservices),
+    path('updateservice/', updateservice),
+    path('deletereq/<int:sid>/', deleteservice),
+    path('chat/<int:cust_id>/<int:service_id>/', srpr_chat),
+    path('updatepassword/', updatepass),
+]
